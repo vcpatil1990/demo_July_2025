de42323e611cefa4b552a7dd19c51aaaddbbb996
vapatil990 <vcp199013@gmail.com>
2025-09-09 20:16:55 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,10 @@
+let patient={
+    name:"Vishal",
+    blood_Group:"B+",
+    Doses:5,
+   isFine:True,
+
+};
+for(let key in patient){
+     console.log(key);
+ }
---
+++ demo2.js
@@ -0,0 +1,19 @@
+
+
+let myCity="Pune city";
+let subcity="Ravet";
+// console.log(myCity.length);
+// console.log(myCity[2]);
+// console.log(myCity.toUpperCase());
+// console.log(myCity.toLowerCase());
+// console.log(myCity.trim());
+// let a=myCity.slice(5,9)
+// console.log(a);
+// let b=myCity+" "+subcity+" "+"pincode"+" "+412101
+// console.log(b)
+// console.log(subcity.replaceAll("t","H"));
+//  let a=myCity.charAt(0);
+// console.log(a);
+// let b=myCity.startsWith("Pune");
+// let c=myCity.endsWith("city");
+//console.log(b);
